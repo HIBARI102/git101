1eca60fcd6aa9cbb3e72f04c6f7a5140ba716401
khunat taweerut <khunuchtaweerut@gmail.com>
2026-01-01 21:09:29 +0700

    create bubble sort

---
+++ git101/bubble_sort.py
@@ -0,0 +1,20 @@
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        swapped = False
+        for j in range(0, n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+                swapped = True
+        
+       
+        if not swapped:
+            break
+    return arr
+
+
+
+if __name__ == "__main__":
+    numbers = list(map(int, input("Enter integer number with space: ").split()))
+    sorted_numbers = bubble_sort(numbers)
+    print("Sorted number is", sorted_numbers)
